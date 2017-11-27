6bae7bb00cdcc94471059bc4a9a3b99b3e73eb2c
pengxingjian.pxj <pengxingjian.pxj@alibaba-inc.com>
2017-11-27 08:41:06 +0800

    update dev-server's port

--- scripts/startDevServer.sh
+++ scripts/startDevServer.sh
@@ -6,4 +6,4 @@ rm -rf ./.site
 rm -rf ./.prerender
 webpack --config "$PWD/site/webpack.prerender.config.js"
 ./scripts/renderPages.sh
-webpack-dev-server --config "$PWD/site/webpack.client.config.js" --no-info --content-base .site --port ${1-8080}
+webpack-dev-server --config "$PWD/site/webpack.client.config.js" --no-info --content-base .site --port ${1-8000}
